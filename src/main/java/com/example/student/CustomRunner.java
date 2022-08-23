e0fe4748f047347b6783a0469e61d4222275c557
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2022-08-24 03:03:42 +0430

    startup task using postconstruct tested

--- src/main/java/com/example/student/CustomRunner.java
+++ src/main/java/com/example/student/CustomRunner.java
@@ -1,11 +1,13 @@
 package com.example.student;
 
+import com.example.student.model.School;
 import com.example.student.model.Student;
 import com.example.student.service.SchoolService;
 import com.example.student.service.StudentService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Map;
 
@@ -22,10 +24,7 @@ public class CustomRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Here");
         Map<String, List<Student>> myMap = studentService.getTopStudentsMap();
-        System.out.println(myMap.get("high").get(0).getName());
-        System.out.println("Here!!!");
         for(String key : myMap.keySet()){
             System.out.println(key + ":");
             for(Student student: myMap.get(key)){
@@ -36,6 +35,21 @@ public class CustomRunner implements CommandLineRunner {
                 System.out.println();
             }
             System.out.println();
+        }
+    }
+
+    @PostConstruct
+    public void postConstructSampleMethod(){
+        School school = schoolService.getSchoolById(2);
+        if(school != null) {
+            Student student = studentService.registerStudent(new Student("Fredric", 19, 17.75, school));
+            System.out.println("\t" + "id: " + student.getId());
+            System.out.println("\t" + "name: " + student.getName());
+            System.out.println("\t" + "grade: " + student.getGrade());
+            System.out.println("\t" + "age: " + student.getAge());
+            System.out.println();
+        }else {
+            System.out.println("Here");
         }
     }
 }
