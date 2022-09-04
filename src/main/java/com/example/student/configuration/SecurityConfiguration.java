4b57cf030555b2cfd0884fb0ba5247fee6cd8451
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2022-09-05 03:47:12 +0430

    making an endpoint accessible to unauthorized users

--- src/main/java/com/example/student/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/student/configuration/SecurityConfiguration.java
@@ -3,6 +3,8 @@ package com.example.student.configuration;
 import com.example.student.service.DatabaseUserDetailService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -13,6 +15,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     public SecurityConfiguration(DatabaseUserDetailService userDetailService) {
         this.userDetailService = userDetailService;
+    }
+//
+//    @Override
+//    public void configure(HttpSecurity http)throws Exception{
+//        http.authorizeRequests()
+//                .antMatchers("/student/all").permitAll()
+//                .anyRequest().authenticated();
+//    }
+
+    @Override
+    public void configure(WebSecurity web)throws Exception{
+        web.ignoring().antMatchers("/student/all");
     }
 
     @Override
