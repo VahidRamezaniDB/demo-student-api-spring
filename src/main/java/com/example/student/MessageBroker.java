e131378c04498d5ffa223eaca66a96caa6da7012
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2022-09-07 05:12:33 +0430

    using rmq service to send messages

--- src/main/java/com/example/student/MessageBroker.java
+++ src/main/java/com/example/student/MessageBroker.java
@@ -1,5 +1,6 @@
 package com.example.student;
 
+import com.example.student.service.RabbitMQSender;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -8,13 +9,16 @@ import org.springframework.amqp.core.Queue;
 
 @Component
 public class MessageBroker implements CommandLineRunner {
-    @Override
-    public void run(String... args) throws Exception {
-        RabbitTemplate rabbitTemplate = new
+
+    private RabbitMQSender rabbitMQSender;
+
+    public MessageBroker(RabbitMQSender rabbitMQSender) {
+        this.rabbitMQSender = rabbitMQSender;
     }
 
-    @Bean
-    public Queue myQueue(){
-        return new Queue("myQueue", false);
+    @Override
+    public void run(String... args) throws Exception {
+        rabbitMQSender.sendText("Hello world!");
+        System.out.println("The sending message task is done.");
     }
 }
