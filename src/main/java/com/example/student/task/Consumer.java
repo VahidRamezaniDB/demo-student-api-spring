049b203bf99c514ad462c4972f09d7cc6baaee04
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2022-09-07 07:01:08 +0430

    implementing message consumer.

--- src/main/java/com/example/student/task/Consumer.java
+++ src/main/java/com/example/student/task/Consumer.java
@@ -1,12 +1,13 @@
 package com.example.student.task;
 
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Consumer implements CommandLineRunner {
-    @Override
-    public void run(String... args) throws Exception {
+public class Consumer{
 
+    @RabbitListener(queues = "myQueue")
+    public void listenForText(String in){
+        System.out.println("Message from myQueue: " + in);
     }
 }
